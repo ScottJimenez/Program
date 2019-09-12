37daaa828d055de46b667009ba0f5f0dc1e79779
ScottJimenez <55092092+ScottJimenez@users.noreply.github.com>
2019-09-12 23:03:22 +0200

    Add files via upload

---
+++ random/random/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace random
+{
+    class Program
+    {
+        public static object Const { get; private set; }
+
+        static void Main(string[] args)
+        {
+
+            //Declaration
+            double _lengteGazon, _breedteGazon, _breedteGrasmaaier, _aantalStroken, _TijdUur, _TijdMinuut;
+            const double SNELHEID = 0.8;
+
+            //Input 
+            Console.WriteLine("Wat is de lengte van het gazon? (m)");
+            _lengteGazon = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Wat is de breedte van het gazon? (m)");
+            _breedteGazon = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Wat is de breedte van de grasmaaier? (cm)");
+            _breedteGrasmaaier = Convert.ToDouble(Console.ReadLine());
+
+            //Omzetten naar meter
+            _breedteGrasmaaier /= 100;
+            //Aantal stroken
+            _aantalStroken = _breedteGazon / _breedteGrasmaaier;
+            //Tijd per strook
+            _TijdUur = _lengteGazon / SNELHEID;
+            //Tijd voor gazon
+            _TijdUur *= _aantalStroken;
+            //Tijd omzetten naar uur
+            _TijdUur /= 3600;
+            //Minuten
+            _TijdMinuut = _TijdUur % 3600;
+            _TijdMinuut *= 60;
+            if (_TijdMinuut > 60)
+            {
+                _TijdMinuut -= _TijdUur * 60;
+            }
+            //Output
+            Console.WriteLine("U zal ongeveer " + _TijdUur.ToString("0 uur") + " en " + _TijdMinuut.ToString("0 minuten") + " over doen.");
+            Console.ReadLine() ;
+
+
+            }
+
+        }
+    }
